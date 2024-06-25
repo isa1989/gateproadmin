2b5923975c255d8a3c7f5af37bfd8cdb99119ca6
isagtech <isar@galltech.az>
2024-06-25 13:48:05 +0400

    add model translation

--- gateproadmin/settings.py
+++ gateproadmin/settings.py
@@ -95,13 +95,13 @@ WSGI_APPLICATION = "gateproadmin.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # if DEBUG:
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-# else:
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
+# # else:
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
