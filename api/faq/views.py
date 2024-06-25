8df3af138ac7f76e9f3e3c4b6e38d8177f4de358
isagtech <isar@galltech.az>
2024-06-25 13:46:35 +0400

    add model translation

--- api/faq/views.py
+++ api/faq/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from faq.models import FAQ
-from faq.serializers import FAQSerializer
+from .serializers import FAQSerializer
 
 
 class FAQListView(ListAPIView):
