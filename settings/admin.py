aff4c35302867c2e0a5f14325be2af87c208e5f9
isagtech <isar@galltech.az>
2024-07-09 17:08:41 +0400

    fix

--- settings/admin.py
+++ settings/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Settings, PlayStoreFormSubmission, PrivacyPolicy, TermsConditions
+from modeltranslation.admin import TranslationAdmin
+
+
+@admin.register(PrivacyPolicy)
+class PrivacyPolicyAdmin(TranslationAdmin):
+    pass
+
+
+@admin.register(TermsConditions)
+class TermsConditionsAdmin(TranslationAdmin):
+    pass
+
 
 admin.site.register(Settings)
 admin.site.register(PlayStoreFormSubmission)
-admin.site.register(PrivacyPolicy)
-admin.site.register(TermsConditions)
