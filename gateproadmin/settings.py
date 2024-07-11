7bc1ae1b90c88a3f784b91c2e5dcbdb51ae72629
isagtech <isar@galltech.az>
2024-07-11 17:49:49 +0400

    integr prod

--- gateproadmin/settings.py
+++ gateproadmin/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i&_0t-+z4ie3u7l(2q3gb2h^x#=frouvd=i_o03sj8kv&n@kj$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
