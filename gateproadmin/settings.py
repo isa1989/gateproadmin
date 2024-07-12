ff549e7d981057a8188d35744485102316c3341f
isagtech <isar@galltech.az>
2024-07-12 12:06:56 +0400

    integr prod

--- gateproadmin/settings.py
+++ gateproadmin/settings.py
@@ -162,10 +162,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static/"
 
-
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
-
+print(STATIC_ROOT, "staticcccccccccccccccccccccccccccccccccccccccc")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
