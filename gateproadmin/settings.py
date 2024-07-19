590ea0dc402b237a7584b5da7fc5065d48de2bbe
isagtech <isar@galltech.az>
2024-07-19 14:04:54 +0400

    upd profile api

--- gateproadmin/settings.py
+++ gateproadmin/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "modeltranslation",
+    "django.contrib.admin",
     "rest_framework",
     "customer",
     "device",
