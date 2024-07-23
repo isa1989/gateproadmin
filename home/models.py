ffb5dc8463cf97df5181fc42b1bd0ef6458f5597
isagtech <isar@galltech.az>
2024-07-23 15:58:34 +0400

    fix firebase and front

--- home/models.py
+++ home/models.py
@@ -5,6 +5,12 @@ class HomePage(models.Model):
     logo = models.ImageField(upload_to="homepage/", blank=True, null=True)
     banner_image = models.ImageField(upload_to="homepage/", blank=True, null=True)
     feature_image = models.ImageField(upload_to="homepage/", blank=True, null=True)
+    whatsapp = models.URLField(
+        blank=True, null=True, help_text="Enter your WhatsApp URL"
+    )
+    instagram = models.URLField(
+        blank=True, null=True, help_text="Enter your Instagram URL"
+    )
 
 
 class ScreenFrame(models.Model):
